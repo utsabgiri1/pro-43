6ce9c07586acc4c410550b5046ddb947c1835ade
utsabgiri1 <78822445+utsabgiri1@users.noreply.github.com>
2021-04-28 15:51:03 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N I N U O F T C I", 700,150)
+    fill("lightblue")
+    text("Hint: Which funCtion performs a spicific task !!", 700,170) 
+    
+    fill("white")
+    textSize(15)
+    text(" B A S T A A E D", 100,250)
+    fill("lightblue")
+    text("Hint: The thing that stores all information !!", 100,270)
+
+}
